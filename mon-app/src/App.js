14c3649bfd5b4227b5e154d4ce792647c17efadf
Jhonnatan7br <jhonnatan7br@gmail.com>
2023-12-12 15:33:27 +0100

    Update

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import logo from './Brandstorm.png';
+import hairImage from './Hair.png'; // Import the image here
+
 import './App.css';
+import './Azure.js';
+import './Azure2.js';
+import React, { useState } from 'react';
+// Import your Azure functions
+import { uploadToAzure, analyzeWithAzure2 } from './Azure';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [jsonResponse, setJsonResponse] = useState(null);
+  const [textResponse, setTextResponse] = useState('');
+
+  const handleFileUpload = event => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const uploadPhotoToAzure = async () => {
+    // Assume uploadToAzure is a function that handles the file upload
+    const json = await uploadToAzure(selectedFile);
+    setJsonResponse(json);
+  };
+
+  const sendJsonToAzure2 = async () => {
+    // Assume analyzeWithAzure2 is a function that sends the json and gets back the text
+    const textJson = await analyzeWithAzure2(jsonResponse);
+    setTextResponse(textJson.text);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="rainbow-text">
+        Look at this incredible recommendation for your Hair
+        </div>
+        <div className="result-box">{textResponse}</div>
+        <img src={hairImage} alt="Hair" /> {/* Add the image here */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://brandstorm.loreal.com/es"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Loreal Braindstorm information
         </a>
       </header>
+      <input type="file" onChange={handleFileUpload} />
+      <button onClick={uploadPhotoToAzure}></button>
+      <button onClick={sendJsonToAzure2} disabled={!jsonResponse}></button>
+      
     </div>
   );
 }
